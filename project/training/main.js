5aa9e27e3166d5391aecac2b9364deddc69d04e7
Admin <sorokivskyi777@gmail.com>
2023-05-07 14:23:03 +0300

    resolved bug

---
+++ project/training/main.js
@@ -0,0 +1,15 @@
+// const obj = {
+//     name: 'vasya',
+//     gender: 'male',
+//     age: 31,
+// };
+//
+// let entries = Object.entries(obj);
+// console.log(entries);
+
+// const arr = ['qwe', 'xxx', 'dddd', 'sss'];
+//
+// const [a, b, ...rest] = arr;
+//
+// console.log(a);
+
